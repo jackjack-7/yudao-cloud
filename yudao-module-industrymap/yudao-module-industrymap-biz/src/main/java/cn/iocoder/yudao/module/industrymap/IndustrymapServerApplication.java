40524bf423ad849e26c87d71fc1f26e72f5c910a
LIANGDELL\liang <liangwinlive@live.com>
2024-11-03 08:39:04 +0800

    init：产业电子地图初始模块添加

---
+++ yudao-module-industrymap/yudao-module-industrymap-biz/src/main/java/cn/iocoder/yudao/module/industrymap/IndustrymapServerApplication.java
@@ -0,0 +1,16 @@
+package cn.iocoder.yudao.module.industrymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 产业电子地图项目的启动类
+ *
+ */
+@SpringBootApplication
+public class IndustrymapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndustrymapServerApplication.class, args);
+    }
+}
---
+++ yudao-module-industrymap/yudao-module-industrymap-biz/src/main/java/cn/iocoder/yudao/module/industrymap/framework/security/config/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package cn.iocoder.yudao.module.industrymap.framework.security.config;
+
+import cn.iocoder.yudao.framework.security.config.AuthorizeRequestsCustomizer;
+import cn.iocoder.yudao.module.infra.enums.ApiConstants;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+/**
+ * 产业电子地图 模块的 Security 配置
+ */
+@Configuration("industrymapSecurityConfiguration")
+public class SecurityConfiguration {
+
+    @Bean("industrymapAuthorizeRequestsCustomizer")
+    public AuthorizeRequestsCustomizer authorizeRequestsCustomizer() {
+        return new AuthorizeRequestsCustomizer() {
+
+            @Override
+            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+                // Swagger 接口文档
+                registry.requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/webjars/**").permitAll()
+                        .requestMatchers("/swagger-ui").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll();
+                // Spring Boot Actuator 的安全配置
+                registry.requestMatchers("/actuator").permitAll()
+                        .requestMatchers("/actuator/**").permitAll();
+                // Druid 监控
+                registry.requestMatchers("/druid/**").permitAll();
+                // RPC 服务的安全配置
+                registry.requestMatchers(ApiConstants.PREFIX + "/**").permitAll();
+            }
+        };
+    }
+}
